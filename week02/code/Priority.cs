73795ff60f724ad79606f4d0d0f18065c7b0e8ef
gawahlb <wah23001@byui.edu>
2024-06-22 02:18:04 -0700

    finished priority question in week 2 assignment

--- week02/code/Priority.cs
+++ week02/code/Priority.cs
@@ -1,5 +1,7 @@
-﻿public static class Priority {
-    public static void Test() {
+﻿public static class Priority
+{
+    public static void Test()
+    {
         // TODO Problem 2 - Write and run test cases and fix the code to match requirements
         // Example of creating and using the priority queue
         var priorityQueue = new PriorityQueue();
@@ -8,20 +10,34 @@
         // Test Cases
 
         // Test 1
-        // Scenario: 
-        // Expected Result: 
+        // Scenario: Test will add people with associated priority; Bob(1), Greg(2), Tim(2), Hal(3). Then it will get rid of Bob and Greg. After this it will queue up 
+        // Gregory(1) and Henry(2).
+        // Expected Result: (Bob, Tim, Gregory, Henry)
         Console.WriteLine("Test 1");
+        var addPeople = new PriorityQueue();
+        addPeople.Enqueue("Bob", 1);
+        addPeople.Enqueue("Greg", 2);
+        addPeople.Enqueue("Tim", 2);
+        addPeople.Enqueue("Hal", 3);
+        addPeople.Dequeue();
+        addPeople.Dequeue();
+        addPeople.Enqueue("Gregory", 1);
+        addPeople.Enqueue("Henry", 2);
 
-        // Defect(s) Found: 
+        Console.WriteLine(addPeople);
+        // Defect(s) Found: Changed index to 0 in Dequeue function. Added (_queue.Remove(_queue[highPriorityIndex]);) 
+        // to dequeue function. Deleted "- 1" from dequeue function if statement. Added if statement nested in a for statement
+        // to find the first instance of a particular priority.
 
         Console.WriteLine("---------");
 
         // Test 2
-        // Scenario: 
-        // Expected Result: 
+        // Scenario: Passing an empty queue to the dequeue function.
+        // Expected Result: Error Message
         Console.WriteLine("Test 2");
-
-        // Defect(s) Found: 
+        addPeople = new PriorityQueue();
+        addPeople.Dequeue();
+        // Defect(s) Found: None
 
         Console.WriteLine("---------");
 

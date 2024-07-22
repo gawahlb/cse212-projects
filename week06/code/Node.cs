62bd1d18715626a4e970fd9f8ccd9cc1a31097cd
gawahlb <wah23001@byui.edu>
2024-07-22 14:01:38 -0700

    finished week 6 code

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -1,36 +1,95 @@
-public class Node {
+public class Node
+{
     public int Data { get; set; }
     public Node? Right { get; private set; }
     public Node? Left { get; private set; }
 
-    public Node(int data) {
+    public Node(int data)
+    {
         this.Data = data;
     }
 
-    public void Insert(int value) {
-        if (value < Data) {
-            // Insert to the left
+    public void Insert(int value)
+    {
+        if (value != Data)
+        {
+            if (value < Data)
+            {
+                // Insert to the left
+                if (Left is null)
+                    Left = new Node(value);
+                else
+                    Left.Insert(value);
+
+            }
+            else
+            {
+                // Insert to the right
+                if (Right is null)
+                    Right = new Node(value);
+                else
+                    Right.Insert(value);
+            }
+        }
+    }
+
+    public bool Contains(int value)
+    {
+        if (Data == value)
+        {
+            return true;
+        }
+        else if (value < Data && Left != null)
+        {
+            return Left.Contains(value);
+        }
+        else if (value > Data && Right != null)
+        {
+            return Right.Contains(value);
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public int GetHeight()
+    {
+
+        if (Left is null && Right is null)
+        {
+            return 1;
+        }
+        else
+        {
+            int leftCount;
+            int rightCount;
+
             if (Left is null)
-                Left = new Node(value);
+            {
+                leftCount = 0;
+            }
             else
-                Left.Insert(value);
-        }
-        else {
-            // Insert to the right
+            {
+                leftCount = Left.GetHeight();
+            }
             if (Right is null)
-                Right = new Node(value);
+            {
+                rightCount = 0;
+            }
             else
-                Right.Insert(value);
+            {
+                rightCount = Right.GetHeight();
+            }
+
+            if (leftCount > rightCount)
+            {
+                return leftCount + 1;
+            }
+            else
+            {
+                return rightCount + 1;
+            }
         }
-    }
-
-    public bool Contains(int value) {
-        // TODO Start Problem 2
-        return false;
-    }
-
-    public int GetHeight() {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
     }
 }
